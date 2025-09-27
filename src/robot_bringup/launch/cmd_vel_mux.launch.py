ed9cd289726fe1604ad734ee86c51d69ee81dd33
Lam Nguyen <lamcnguyen89@gmail.com>
2025-09-27 11:42:26 -0400

    Added instructions to run cmd_vel_mux

--- src/robot_bringup/launch/cmd_vel_mux.launch.py
+++ src/robot_bringup/launch/cmd_vel_mux.launch.py
@@ -1,26 +1,14 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
 import os
 
 
 def generate_launch_description():
-    # Get the path to the config file
-    config_file = os.path.join(
-        get_package_share_directory('robot_bringup'),
-        'config',
-        'cmd_vel_mux.yaml'
-    )
-    
     return LaunchDescription([
         Node(
-            package='cmd_vel_mux',
-            executable='cmd_vel_mux',
-            name='cmd_vel_mux',
+            package='robot_bringup',
+            executable='simple_cmd_vel_mux.py',
+            name='simple_cmd_vel_mux',
             output='screen',
-            parameters=[config_file],
-            remappings=[
-                ('cmd_vel_out', '/cmd_vel'),
-            ]
         ),
     ])
